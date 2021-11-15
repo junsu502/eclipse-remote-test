4517df2464d10273cb9e28fc0bf1e9825b44d95f
junsu502 <94041577+junsu502@users.noreply.github.com>
2021-11-15 15:51:30 +0900

    Update Start.java
    fetch

--- HelloWorld/src/Start.java
+++ HelloWorld/src/Start.java
@@ -4,7 +4,7 @@ public class Start {
 	public static void main(String[] args) {
          System.out.println("Hello World");
          System.out.println("Slave Branch");
-		
+//   	fetch accept?
          
 //         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
          
